bc573307a131abe3d6342afb444a8d951071b9ac
quandle-cyber <quandledingle270@gmail.com>
2025-08-03 13:35:38 -0700

    Upload script.js via Script Source Converter

---
+++ script.js
@@ -0,0 +1,10 @@
+Null Obfuscator
+Obfuscation failed: Obfuscation process failed with exit code 1. STDOUT: "[0m[35mPROMETHEUS: [0mApplying Obfuscation Pipeline to C:\Users\ADMINI~1\AppData\Local\Temp\2\tmp-576-3E2YAXMX4O69-.lua ... [0m[35mPROMETHEUS: [0mParsing ... [0m[31mPROMETHEUS: Parsing Error at Position 11:60, expected a ";" or a ","[0m" STDERR: "./bin/luajit.exe: ./lua/logger.lua:54: Parsing Error at Position 11:60, expected a ";" or a "," stack traceback: [C]: in function 'error' ./lua/logger.lua:54: in function 'errorCallback' ./lua/logger.lua:57: in function 'error' ./lua/prometheus\parser.lua:962: in function 'tableOrFunctionLiteral' ./lua/prometheus\parser.lua:713: in function 'expressionUnary' ./lua/prometheus\parser.lua:667: in function 'expressionMulDivMod' ./lua/prometheus\parser.lua:645: in function 'expressionAddSub' ./lua/prometheus\parser.lua:634: in function 'expressionStrCat' ./lua/prometheus\parser.lua:590: in function 'expressionComparision' ./lua/prometheus\parser.lua:579: in function 'expressionAnd' ./lua/prometheus\parser.lua:568: in function 'expression' ./lua/prometheus\parser.lua:503: in function 'exprList' ./lua/prometheus\parser.lua:474: in function 'statement' ./lua/prometheus\parser.lua:171: in function 'block' ./lua/prometheus\parser.lua:149: in function 'parse' ./lua/prometheus\pipeline.lua:178: in function 'apply' ./lua/cli.lua:148: in main chunk [C]: at 0x7ff751634950"
+Upload Lua File (max 40KB)
+Or Paste Lua Code
+Select Obfuscation Preset
+          Weak
+          Medium
+        
+Obfuscate Code
+For advanced features, try the standalone version of Null Obfuscator.
